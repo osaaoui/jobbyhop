c70c48b2e3e1cd72e319783d06ff40898ea09d54
omar saaoui <saaoui@gmail.com>
2019-10-11 17:34:18 -0400

    build

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/api/customers', (req, res) => {
+const publiPath = path.join(__dirname, "..", "public");
+app.use(express.static(publiPath));
+
+app.get("/api/customers", (req, res) => {
   const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    { id: 1, firstName: "John", lastName: "Doe" },
+    { id: 2, firstName: "Brad", lastName: "Traversy" },
+    { id: 3, firstName: "Mary", lastName: "Swanson" }
   ];
 
   res.json(customers);
 });
 
-const port = 5000;
+app.get("*", function(req, res) {
+  res.sendFile(path.join(publiPath, "index.html"));
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
