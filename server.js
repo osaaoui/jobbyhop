a51731a98d373c5db11bef596250a434826d8f30
omar saaoui <saaoui@gmail.com>
2019-10-11 18:32:58 -0400

    asdfsadsd

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
-const publicPath = path.join(__dirname, "..", "public");
+app.use(express.static("build"));
 
 app.get("/api/customers", (req, res) => {
   const customers = [
@@ -15,8 +14,8 @@ app.get("/api/customers", (req, res) => {
   res.json(customers);
 });
 
-/*app.get("*", function(req, res) {
-  res.sendFile(path.join(publicPath, "index.html"));
-});*/
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve("build", "index.html"));
+});
 
 app.listen(process.env.PORT || 5000);
