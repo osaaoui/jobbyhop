6c1f09ca2108db7b239914ffb2f001e15876ba81
omar saaoui <saaoui@gmail.com>
2019-10-11 18:29:48 -0400

    sdfsdf

--- server.js
+++ server.js
@@ -19,16 +19,4 @@ app.get("/api/customers", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });*/
 
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
-
-  // Express serve up index.html file if it doesn't recognize route
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(process.env.PORT || 5000);
