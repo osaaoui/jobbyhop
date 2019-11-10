102796e92df940b11f2e711e665096aad8144c49
omar saaoui <saaoui@gmail.com>
2019-11-10 00:25:48 -0500

    removed serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { InfoProvider } from "./components/context";
-import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
