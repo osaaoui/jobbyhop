cc1c6d114b367de0af6e72c0b47a003d663486a3
omar saaoui <saaoui@gmail.com>
2019-11-13 00:23:40 -0500

    modified home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -6,15 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row mt-5">
-          <InfoConsumer>
-            {value => {
-              return value.info.map(item => {
-                return <Info key={item.id} item={item} />;
-              });
-            }}
-          </InfoConsumer>
-        </div>
+        <Info />
       </div>
     );
   }
